55834a52cfaaf8670349ac557bb30c7cd97b1b85
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-10-21 23:36:49 +0100

    Initial commit

---
+++ src/main/java/ma/elamrani/kafkaspringcloudsteam/KafkaSpringCloudSteamApplication.java
@@ -0,0 +1,13 @@
+package ma.elamrani.kafkaspringcloudsteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringCloudSteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringCloudSteamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/elamrani/kafkaspringcloudsteam/KafkaSpringCloudSteamApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.elamrani.kafkaspringcloudsteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringCloudSteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
